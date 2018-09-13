0f75c61f56ee1c6b7834031d970d70aee8a7891b
Mehdi aissa BELALOUI <mbelalou@e2r13p10.42.fr>
2018-09-13 15:50:44 +0200

    13-09-18

--- champs/ex.s
+++
@@ -1,6 +0,0 @@
-.name "sasdqwertyuiop;lkjhgfdsazxcvbngfdsaSQWSDWAKDVBCVAJC GWAVJXYHWCAXCAYTAWCYAWD ASSADASDASDASDSADSADASDASDASFASFASDCADVUJADBUAWIsaaB"
-.comment "commenti"
-
-live %5
-aff r2
-and r5,r1,r2
