879d78d00e6d7ebdfe21af09bbc77f8f05c77e14
Mehdi aissa BELALOUI <mbelalou@e3r3p1.42.fr>
2018-10-03 16:35:18 +0200

    26-09-18

--- champs/e.s
+++
@@ -1,5 +0,0 @@
-.name "sasdqwertyuiop;lkjhgfdsazxcvbngfdsaSQWSDWAKDVBCVAJC GWAVJXYHWCAXCAYTAWCYAWD ASSADASDASDASDSADSADASDASDASFASFASDCADVUJADBUAWIsaaB"
-.comment "commenti"
-
-and r1,r1,r2
-#live %5
