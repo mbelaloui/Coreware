3c385e37acdbcff19a3a724f207f1b9dd9071ce5
mbelaloui <mbelalou@student.42.fr>
2018-10-12 17:36:38 +0200

    ubuntu

--- src/vm/inst/ft_st.c
+++ src/vm/inst/ft_st.c
@@ -6,7 +6,7 @@
 /*   By: mint <mint@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/12 11:47:43 by mint              #+#    #+#             */
-/*   Updated: 2018/10/12 16:16:24 by mint             ###   ########.fr       */
+/*   Updated: 2018/10/12 17:31:39 by mint             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,14 +54,16 @@ void	put_process(t_process *process)
 BOOL	ft_st(t_vm *vm, t_process *process)
 {
 //	t_op	*op_tab[NBR_OP];
-	int val;
+	unsigned int val;
 	put_process(process);
+
+ft_printf("param 1 = %d", process->curent_instruction.vale_arg[0]);
 
 if (process->curent_instruction.vale_arg[0])
 	process->carry = 0;
 else
 	process->carry = 1;
-val = process->reg[process->curent_instruction.vale_arg[0] - 1] % MEM_SIZE;
+val = process->reg[process->curent_instruction.vale_arg[0] - 1];
 if (process->curent_instruction.type_arg[1] == REG_CODE) //valeur a verifer aller dans la meme fonction pour tester si regNUm n'est pas superieur a ce qui est fixer
 	process->reg[process->curent_instruction.vale_arg[1]] = val;
 else
@@ -70,12 +72,13 @@ else
 	ft_printf("\n-------------------------------------\n");
 	ft_printf("\n-------------------------------------\n");
 	ft_printf("\n-------------------------------------\n");
-	ft_printf("\n-------------------------------------\n");
+	ft_printf("\n-----------val = %hhd------------\n", val);
 
-	unsigned char * tab = ft_int_to_byts( (unsigned)val, DIR_SIZE);
+	unsigned char * tab = ft_int_to_byts((unsigned)val, DIR_SIZE);
 	int pt = -1;
 	while (++pt < DIR_SIZE)
 	{
+			ft_printf("\n--------------%.2x-------------------\n", tab[pt]);
 		vm->mem[process->curent_instruction.vale_arg[1] + pt + process->pc - DIR_SIZE % MEM_SIZE][MEM_SRC] = tab[pt];//process->curent_instruction.vale_arg[0] % MEM_SIZE;
 		vm->mem[process->curent_instruction.vale_arg[1] + pt + process->pc - DIR_SIZE % MEM_SIZE][MEM_DESC] = process->id_parent;//process->curent_instruction.vale_arg[0] % MEM_SIZE;
 	}
--- text1.s
+++ text1.s
@@ -1,6 +1,6 @@
 .name "1"
 .comment "1"
 
-ld	3,r2
-st	r2,r1
-st	r2,64
+#ld	123,r2
+#st	r2,r1
+st	r1,64
